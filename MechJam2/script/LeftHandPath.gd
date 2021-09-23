379900ed7b761c981d42bdd09e3b0b497e8e14f3
tamamiyasita <sigorou@gmail.com>
2021-09-23 22:28:22 +0900

    cmt

--- MechJam2/script/LeftHandPath.gd
+++ MechJam2/script/LeftHandPath.gd
@@ -1,7 +1,15 @@
 extends Path2D
 
-onready var _path_follow: PathFollow2D = $PathFollow2D
+onready var _path_follow : PathFollow2D = $PathFollow2D
 onready var left_hand : KinematicBody2D = $PathFollow2D/LeftHand
+onready var left_target  : Area2D = $PathFollow2D/LeftTarget
+
+export(float, 0, 1, 0.001) var speeds
+export(Curve) var speed_curve
+
+var target := Vector2()
+var velocity := Vector2()
+var speed
 
 
 func _ready() -> void:
@@ -9,10 +17,16 @@ func _ready() -> void:
 	
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		var point :Vector2 = event.position
-		curve.clear_points()
-		curve.add_point(point)
+	if event.is_action_pressed('L_click'):
+		target = get_global_mouse_position()
+	left_target.global_position = get_global_mouse_position()
+#	left_hand.global_position = get_global_mouse_position()
 
 func _process(delta: float) -> void:
-	left_hand.move_and_slide(curve.get_point_position(0))
+	speed = speed_curve.interpolate(speeds)
+	velocity = ((target - left_hand.global_position) * (speed)).normalized()
+	left_hand.rotation = velocity.angle()
+	if (target - left_hand.global_position).length() > 5:
+#		velocity = left_hand.move_and_slide(velocity)
+		left_hand.global_position += velocity*5
+
